48d128920ceaa1f5814fff965d25aac2164891fe
Xavier <861900161@qq.com>
2022-02-17 16:14:07 +0800

    增加获取百度热搜功能

---
+++ Amadeus/src/plugins/baidu_hot/__init__.py
@@ -0,0 +1,17 @@
+from nonebot import on_command
+from nonebot.matcher import Matcher
+from nonebot.adapters.onebot.v11 import Message,MessageSegment
+from .spider import get_content
+
+matcher=on_command("热搜",aliases={"hot"},priority=2)
+
+@matcher.handle()
+async def calculate(matcher: Matcher):
+    matcher.stop_propagation()
+    titles =await get_content()
+    await matcher.send("====百度热搜榜Top15====")
+    res=""
+    for i in range(30):
+        res+=Message(f"{i+1}. {titles[i]}\n")
+
+    await matcher.finish(res)
---
+++ Amadeus/src/plugins/baidu_hot/spider.py
@@ -0,0 +1,19 @@
+import requests
+from lxml import etree
+URL="https://top.baidu.com/board?tab=realtime"
+HEADERS={
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Edg/98.0.1108.55'
+}
+
+async def get_content():
+    req=requests
+
+    res=req.get(url=URL,headers=HEADERS)
+    tree=etree.HTML(res.text)
+    titles=tree.xpath('/html/body/div/div/main/div[2]/div/div[2]/div/div[2]/a/div[1]/text()')
+    # links=tree.xpath('/html/body/div/div/main/div[2]/div/div[2]/div/div[2]/a/@href ')
+
+    return titles #,links[:15]
+
+if __name__=='__main__':
+    print()
