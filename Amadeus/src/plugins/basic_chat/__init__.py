b9a2bb1cd7967bfbc997070488937e4da7d61275
xiaoyu2018 <861900161@qq.com>
2022-03-03 21:42:14 +0800

    修复bc中无法使用表情的bug

--- Amadeus/src/plugins/basic_chat/__init__.py
+++ Amadeus/src/plugins/basic_chat/__init__.py
@@ -1,32 +1,34 @@
 from nonebot import get_driver, on_command, on_message
-from nonebot.params import EventPlainText,CommandArg
+from nonebot.params import CommandArg,EventMessage
 from nonebot.matcher import Matcher
 from nonebot.adapters.onebot.v11 import Message,MessageSegment
-from .config import Config
+from os.path import expanduser,join
 
 
-global_config = get_driver().config
-plugin_config = Config(**global_config.dict())
+data_path=join(expanduser('~'),"bot\chat_data.txt")
 
 matcher1=on_message(priority=3)
 matcher2=on_command("记忆",aliases={"ln"},priority=2)
 
 @matcher1.handle()
-async def basic_chat(matcher: Matcher,msg: str = EventPlainText()):
-    chat_data=load_data(plugin_config.bc_data_path)
+async def basic_chat(matcher: Matcher,msg: Message = EventMessage()):
+    chat_data=load_data(data_path)
+    msg=str(msg)
+    if(msg==None or msg==''):
+        return
     if(msg in chat_data.keys()):
-        await matcher.finish(chat_data[msg])
-    # await matcher.finish("666")
+        
+        await matcher.finish(Message(chat_data[msg]))
 
 
 @matcher2.handle()
-async def Add_Data(matcher: Matcher,msg: Message = CommandArg()):
-    chat_data=load_data(plugin_config.bc_data_path)
+async def add_data(matcher: Matcher,msg: Message = CommandArg()):
+    chat_data=load_data(data_path)
     s_msg=str(msg).split(" ")
     # print(s_msg.split(" "))
     chat_data[s_msg[0]]=" ".join(s_msg[1:])
     
-    await save_data(plugin_config.bc_data_path,chat_data)
+    await save_data(data_path,chat_data)
     matcher.stop_propagation()
     await matcher.finish("边人记住了"+Message(MessageSegment.face(16)))
 
@@ -35,13 +37,13 @@ async def save_data(path:str,data:dict):
         for i in data.keys():
             f.write(i+" "+data[i]+"\n")
 
-    # 在服务器备份chatdata，防止部署新版本时丢失文件
-    try:
-        with open("/root/bot/chat_data.txt.bak","w",encoding="utf-8") as f:
-            for i in data.keys():
-                f.write(i+" "+data[i]+"\n")
-    except:
-        print("备份失败！")
+    # # 在服务器备份chatdata，防止部署新版本时丢失文件
+    # try:
+    #     with open("/root/bot/chat_data.txt.bak","w",encoding="utf-8") as f:
+    #         for i in data.keys():
+    #             f.write(i+" "+data[i]+"\n")
+    # except:
+    #     print("备份失败！")
         
 def load_data(path:str):
     chat_data=dict()
--- Amadeus/src/plugins/basic_chat/temp.py
+++ Amadeus/src/plugins/basic_chat/temp.py
@@ -1,1 +1,5 @@
+from os.path import expanduser,join
 
+
+data_path=join(expanduser('~'),"bot\chat_data.txt")
+print(data_path)
